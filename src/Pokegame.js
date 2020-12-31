4f3b2016242a72ee22b51f60ed2636f67c19283f
Jin Cheng <jincheng411@gmail.com>
2020-12-31 01:49:54 -0500

    added Pokegame

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Pokedex from './Pokedex';
 class Pokegame extends Component {
   static defaultProps = {
     pokemon: [
@@ -14,9 +14,19 @@ class Pokegame extends Component {
     ]
   };
   render() {
+    let hand1 = [];
+    let hand2 = [...this.props.pokemon];
+    while(hand1.length < hand2.length) {
+      let randIdx = Math.floor(Math.random() * hand2.length);
+      let pokemon = hand2.splice(randIdx, 1)[0];
+      hand1.push(pokemon);
+    }
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
     return(
       <div className="Pokegame">
-
+        <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2}/>
+        <Pokedex pokemon={hand2} exp={exp2} isWinner={exp1 < exp2}/>
       </div>
     )
   }
